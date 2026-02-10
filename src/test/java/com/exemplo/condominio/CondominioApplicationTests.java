6c12e297d1c73cd1513ef0cd5ee8387b032cda17
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2026-02-09 22:30:40 -0300

    fazendo mais correções

--- src/test/java/com/exemplo/condominio/CondominioApplicationTests.java
+++ src/test/java/com/exemplo/condominio/CondominioApplicationTests.java
@@ -1,5 +1,6 @@
 package com.exemplo.condominio;
 
+import com.exemplo.condominio.infrastructure.BaseIntegrationTest;
 import com.exemplo.condominio.infrastructure.config.TestContainersConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestContainersConfiguration.class)
 @SpringBootTest
-class CondominioApplicationTests {
+class CondominioApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
